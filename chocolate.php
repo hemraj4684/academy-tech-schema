f828ad7b1381571ef31bc8eaf93b29a4bf23f51e
Hemraj Jibhau Solanki <siya21211@gmail.com>
2016-04-17 16:56:41 +0530

    Update chocolate.php

--- chocolate.php
+++ chocolate.php
@@ -1,14 +1,14 @@
 <?php 
 $W = 3; $TC = 0; $RS = 15;
 
-function wrapperChocolate($RS,$W)
+function wrapperChocolate($RS,$W)//function for calculating extra choclates we get against extra wrapper
 {
 	$WC = $TC = $RS;
 	do {
-		$EC = intval($WC/$W);
-		$EW = intval($WC%$W);
-		$WC = $EC+$EW;
-		$TC = $TC + $EC;
+		$EC = intval($WC/$W); //extra choc agaist wrapper
+		$EW = intval($WC%$W);//extra wrapper
+		$WC = $EC+$EW;//total remaining wrapper
+		$TC = $TC + $EC; //total choc
 	} while (($EC+$EW)>=$W);
 	return $TC;
 }
